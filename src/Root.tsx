1c63e933f4a73b4ef7bb077fa2c1eec5250574bd
Rahul Sachdeva <sachnoor2@gmail.com>
2026-06-12 19:37:59 +0530

    Add src/Root.tsx

---
+++ src/Root.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Composition } from 'remotion';
+import { HypnicJerk } from './HypnicJerk';
+
+export const RemotionRoot: React.FC = () => (
+  <>
+    <Composition
+      id="HypnicJerk"
+      component={HypnicJerk}
+      durationInFrames={2700}
+      fps={60}
+      width={1080}
+      height={1920}
+    />
+  </>
+);
